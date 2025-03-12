6080467006fe491cf80883d5029429cf22ab6d91
YU-WEI,HSU <blackfire106.1231@gmail.com>
2025-03-12 16:57:47 +0800

    Define free list allocator API
    Implement init_fl(size_t size) to
    initialize memory limit.
    Implement fl_alloc(size_t size) for memory allocation.
    Implement fl_free(void *ptr) for memory deallocation.
    Define block_t structure for managing memory blocks.
    Add root_block as the entry point of the free list.

---
+++ includes/free_list_alloc.h
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdlib.h>
+typedef struct block {
+    size_t size;
+    struct block *next, *prev;
+    struct block *l, *r;
+} block_t;
+
+
+block_t *root_block = NULL;
+
+/**
+ * init_fl - Initialize the maximum memory limit 
+ * available for the free list allocator.
+ * @size: max memory size
+ */
+void init_fl(size_t size);
+
+/**
+ * fl_alloc - Allocate memory based on the size 
+ * provided by the user and return a pointer.
+ * If the allocation fails, return NULL.
+ * @size: size provided by the user
+ */
+void *fl_alloc(size_t size);
+
+/**
+ * fl_free - Free the memory corresponding to 
+ * the pointer provided by the user.
+ * @ptr: target memory to free
+ */
+void fl_free(void *ptr);
